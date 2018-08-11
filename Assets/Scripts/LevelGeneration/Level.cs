7351b28625e229e422e7bd49694090e334a508e4
Francis Zinsli <Zinsli.alec@gmail.com>
2018-08-10 18:20:23 -0700

    added basic room and cave generation

--- Assets/Scripts/LevelGeneration/Level.cs
+++ Assets/Scripts/LevelGeneration/Level.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Level : MonoBehaviour {
+public class Level : MonoBehaviour
+{
+    public int tileCount = 50;
+    public int pixelsPerTile = 20;
+    public float scale = 0.2f;
+    public int roomCount = 10;
+    public GenerationType type = GenerationType.Mix;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private int textureSize;
+    SpriteRenderer r;
+    LevelGenerator gen;
+
+    // Use this for initialization
+    void Start()
+    {
+        r = GetComponent<SpriteRenderer>();
+        gen = new LevelGenerator(tileCount, tileCount, pixelsPerTile, roomCount);
+        textureSize = tileCount * pixelsPerTile;
+        addTexture();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void addTexture()
+    {
+        Texture2D tex = gen.generateTexture(scale, type);
+        Rect re = new Rect(0, 0, textureSize, textureSize);
+        r.sprite = Sprite.Create(tex, re, new Vector2(0.5f, 0.5f));
+        //gameObject.AddComponent<PolygonCollider2D>();
+    }
 }
